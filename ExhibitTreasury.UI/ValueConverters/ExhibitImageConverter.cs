428ecc58511b5d306e443c8d7b14d94303a8a96c
3Matvey <andrianov8m@gmail.com>
2025-05-13 02:35:07 +0300

    fix visibility

---
+++ ExhibitTreasury.UI/ValueConverters/ExhibitImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Maui.Controls;
+
+namespace ExhibitTreasury.UI.ValueConverters
+{
+    public class ExhibitImageConverter : IValueConverter
+    {
+        // Если файл <AppData>/Images/{Id}.jpg есть – возвращаем его, иначе placeholder
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int id)
+            {
+                var imagesDir = Path.Combine(FileSystem.AppDataDirectory, "Images");
+                var file = Path.Combine(imagesDir, $"{id}.jpg");
+                if (File.Exists(file))
+                    return ImageSource.FromFile(file);
+            }
+            // Положите в Resources/Images файл placeholder.jpg (BuildAction = MauiImage)
+            return ImageSource.FromFile("placeholder.jpg");
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
